d0da97377db925f6c4607efb1124c901cd87104f
gabr1el1 <gabrielhgxd@gmail.com>
2023-04-30 16:36:55 -0600

    Path relativo para la bd

--- uMain.pas
+++ uMain.pas
@@ -96,7 +96,7 @@ begin
  // ubicacion de la bd
  {$IF DEFINED(MSWINDOWS)}
  // windows
- dbFileName := 'C:\isc\Octavo semestre\ProgramacionMovil\Projects\SQLiteDemo\escolar.db';
+ dbFileName := '.\escolar.db';
  {$ELSE}
    // Android, iOS, Mac
    dbFileName := TPath.Combine(TPath.GetDocumentsPath,'escolar.db');
